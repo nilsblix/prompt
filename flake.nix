8aa95db4a45782cb72ad98a4fcf1d6ab51141d39
nilsblix <nilsblix06@gmail.com>
2025-06-20 18:02:31 +0200

    flake.nix

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
                 devShells.default = pkgs.mkShell {
                     packages = [
                         pkgs.cargo
+                        pkgs.rustc
                     ];
                 };
 
