a5bd19546d99a433f7c3500da9cdcca0fd0c0462
nilsblix <nilsblix06@gmail.com>
2025-06-20 15:37:17 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,289 @@
+use std::{
+    env, 
+    error::Error, 
+    fmt::{ self, Write, }, 
+};
+
+#[allow(unused)]
+enum Color {
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+}
+
+impl Color {
+    fn to_ansi(&self) -> i32 {
+        match self {
+            Color::Red => 31,
+            Color::Green => 32,
+            Color::Yellow => 33,
+            Color::Blue => 34,
+            Color::Magenta => 35,
+            Color::Cyan => 36,
+            Color::White => 37,
+        }
+    }
+}
+
+enum DecoratedString {
+    Bold(Box<DecoratedString>),
+    Colored(Box<DecoratedString>, Color),
+    Default(String),
+}
+
+impl DecoratedString {
+    fn append_to_ansi(val: &DecoratedString, s: &mut String) -> Result<(), fmt::Error> {
+        // https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+        match val {
+            DecoratedString::Bold(inner) => {
+                write!(s, "\x1b[1m")?;
+                Self::append_to_ansi(inner, s)?;
+                write!(s, "\x1b[22m")?;
+            }
+            DecoratedString::Colored(inner, color) => {
+                write!(s, "\x1b[{}m", color.to_ansi())?;
+                Self::append_to_ansi(inner, s)?;
+                write!(s, "\x1b[39m")?;
+            }
+            DecoratedString::Default(val) => {
+                write!(s, "{val}")?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn to_ansi(&self) -> String {
+        let mut ret = String::new();
+        Self::append_to_ansi(self, &mut ret).unwrap();
+        ret
+    }
+
+    fn bold(self) -> DecoratedString {
+        DecoratedString::Bold(Box::new(self))
+    }
+
+    fn colored(self, color: Color) -> DecoratedString {
+        DecoratedString::Colored(Box::new(self), color)
+    }
+
+    fn new(s: String) -> DecoratedString {
+        DecoratedString::Default(s)
+    }
+}
+
+fn get_time() -> String {
+    let time = format!("{}", chrono::Local::now().time().format("%H:%M"));
+    DecoratedString::new(time)
+        .bold()
+        .colored(Color::Cyan)
+        .to_ansi()
+}
+
+#[derive(Debug)]
+enum UserError {
+    NoUser(std::io::Error),
+}
+
+impl Error for UserError {}
+
+impl fmt::Display for UserError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            UserError::NoUser(e) => write!(f, "failed to get user: {}", e), 
+        }
+    }
+}
+
+fn get_user() -> Result<String, UserError> {
+    let user = whoami::fallible::username().map_err(UserError::NoUser)?;
+    let dec = DecoratedString::new(user)
+        .bold()
+        .colored(Color::Magenta)
+        .to_ansi();
+    Ok(dec)
+}
+
+#[derive(Debug)]
+enum HostnameError {
+    NoHost(std::io::Error),
+}
+
+impl Error for HostnameError {}
+
+impl fmt::Display for HostnameError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            HostnameError::NoHost(e) => write!(f, "failed to get host: {}", e), 
+        }
+    }
+}
+
+fn get_hostname() -> Result<String, HostnameError> {
+    let host = whoami::fallible::hostname().map_err(HostnameError::NoHost)?;
+    let dec = DecoratedString::new(host)
+        .bold()
+        .colored(Color::Green)
+        .to_ansi();
+    Ok(dec)
+}
+
+fn get_cwd() -> String  {
+    let cwd = env::var("PWD");
+
+    if cwd.is_err() {
+        return DecoratedString::new("!!!".to_string())
+            .bold()
+            .colored(Color::Red)
+            .to_ansi();
+    }
+
+    let mut cwd = cwd.unwrap();
+    
+    if let Ok(home) = env::var("HOME") {
+        if cwd.starts_with(&home) {
+            cwd = cwd.replace(&home, "~");
+        }
+    }
+
+    DecoratedString::new(cwd)
+        .bold()
+        .colored(Color::Cyan)
+        .to_ansi()
+}
+
+#[derive(Debug)]
+enum ShellError {
+    EnvNotSet,
+    NoFileName,
+}
+
+impl Error for ShellError {}
+
+impl fmt::Display for ShellError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ShellError::EnvNotSet => write!(f, "shell env not set"),
+            ShellError::NoFileName => write!(f, "failed to get shell name"),
+        }
+    }
+}
+
+fn get_shell() -> Result<String, ShellError> {
+    let shell: std::path::PathBuf = env::var("SHELL")
+        .map_err(|_| ShellError::EnvNotSet)?
+        .into();
+
+    let name = shell
+        .file_name()
+        .ok_or(ShellError::NoFileName)?
+        .to_string_lossy();
+
+    Ok(DecoratedString::new(name.to_string())
+        .bold()
+        .colored(Color::White)
+        .to_ansi())
+}
+
+#[derive(Debug)]
+struct NotInNixShell{}
+
+impl Error for NotInNixShell {}
+
+impl fmt::Display for NotInNixShell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "not in a nix shell")
+   }
+}
+
+fn get_nix_shell() -> Result<String, NotInNixShell> {
+    env::var("IN_NIX_SHELL").map_err(|_| NotInNixShell{})?;
+
+    let shell_name = std::env::var("name").unwrap_or("nix-shell".to_string());
+
+    Ok(DecoratedString::new(format!("nix: {}", {shell_name}))
+        .bold()
+        .to_ansi())
+}
+
+#[derive(Debug)]
+enum MainError {
+    User(UserError),
+    Hostname(HostnameError),
+    Shell(ShellError),
+    NixShell(NotInNixShell),
+}
+
+impl fmt::Display for MainError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let source: &dyn std::error::Error = match self {
+            MainError::User(e) => {
+                writeln!(f, "failed to get user info")?;
+                e
+            },
+            MainError::Hostname(e) => {
+                writeln!(f, "failed to get hostname info")?;
+                e
+            },
+            MainError::Shell(e) => {
+                writeln!(f, "failed to get shell info")?;
+                e
+            },
+            MainError::NixShell(e) => {
+                writeln!(f, "failed to get nix info")?;
+                e
+            },
+        };
+
+        writeln!(f, "Caused by:")?;
+
+        let mut source = Some(source);
+        while let Some(err) = source {
+            writeln!(f, "{err}")?;
+            source = err.source();
+        }
+
+        Ok(())
+    }
+}
+
+fn do_print(mut components: Vec<String>) {
+    components.insert(0, "┌[".into());
+    for i in 1..components.len() - 1 {
+        components.insert(2 * i, "]-[".into());
+    }
+    components.push("]\n└> ".into());
+    for component in components {
+        print!("{component}");
+    }
+}
+
+fn main() {
+    let (oks, errors): (Vec<Result<_, MainError>>, Vec<_>) = vec![
+        Ok(get_time()),
+        get_user().map_err(MainError::User),
+        get_hostname().map_err(MainError::Hostname),
+        Ok(get_cwd()),
+        get_shell().map_err(MainError::Shell),
+        get_nix_shell().map_err(MainError::NixShell),
+    ]
+    .into_iter()
+    .partition(Result::is_ok);
+
+    let components: Vec<_> = oks
+        .into_iter()
+        .map(|x| x.expect("Invalid Result"))
+        .collect();
+
+    if Ok("1") == env::var("DEBUG_PROMPTLINE").as_ref().map(|s| s.as_str()) {
+        for error in errors.into_iter().map(|e| e.unwrap_err()) {
+            eprintln!("{error}");
+        }
+    }
+
+    do_print(components);
+}
