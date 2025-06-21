0612af068575b60d8ca5f0d2230fa7336ce3a2c1
nilsblix <nilsblix06@gmail.com>
2025-06-21 15:33:35 +0200

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs {
+                    inherit system;
+                };
+            in {
+                devShells.default = pkgs.mkShell {
+                    packages = with pkgs; [
+                        cargo rustc
+                    ];
+                };
+
+                packages.default = pkgs.rustPlatform.buildRustPackage {
+                    pname = "prompt";
+                    version = "0.1.0";
+                    src = ./.;
+                    cargoLock = {
+                        lockFile = ./Cargo.lock;
+                    };
+                };
+            }
+        );
+}
