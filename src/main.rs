3fefa6f754bb2c85caaf31349ea4e8d5e3ef3657
nilsblix <nilsblix06@gmail.com>
2025-07-09 23:31:16 +0200

    I give up

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,88 @@
 use std::{
-    env,
-    error::Error,
-    fmt::{ self },
+    env, 
+    error::Error, 
+    fmt::{ self, Write, },
 };
 
-use nu_ansi_term::Color;
+#[allow(unused)]
+enum Color {
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+}
+
+impl Color {
+    fn to_ansi(&self) -> i32 {
+        match self {
+            Color::Red => 31,
+            Color::Green => 32,
+            Color::Yellow => 33,
+            Color::Blue => 34,
+            Color::Magenta => 35,
+            Color::Cyan => 36,
+            Color::White => 37,
+        }
+    }
+}
+
+enum DecoratedString {
+    Bold(Box<DecoratedString>),
+    Colored(Box<DecoratedString>, Color),
+    Default(String),
+}
+
+impl DecoratedString {
+    fn append_to_ansi(val: &DecoratedString, s: &mut String) -> Result<(), fmt::Error> {
+        // https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+        match val {
+            DecoratedString::Bold(inner) => {
+                write!(s, "\x1b[1m")?;
+                Self::append_to_ansi(inner, s)?;
+                write!(s, "\x1b[22m")?;
+            }
+            DecoratedString::Colored(inner, color) => {
+                write!(s, "\x1b[{}m", color.to_ansi())?;
+                Self::append_to_ansi(inner, s)?;
+                write!(s, "\x1b[39m")?;
+            }
+            DecoratedString::Default(val) => {
+                write!(s, "{val}")?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn to_ansi(&self) -> String {
+        let mut ret = String::new();
+        Self::append_to_ansi(self, &mut ret).unwrap();
+        ret
+    }
+
+    fn bold(self) -> DecoratedString {
+        DecoratedString::Bold(Box::new(self))
+    }
+
+    fn colored(self, color: Color) -> DecoratedString {
+        DecoratedString::Colored(Box::new(self), color)
+    }
+
+    fn new(s: String) -> DecoratedString {
+        DecoratedString::Default(s)
+    }
+}
+
+fn get_time() -> String {
+    let time = format!("{}", chrono::Local::now().time().format("%H:%M"));
+    DecoratedString::new(time)
+        .bold()
+        .colored(Color::Cyan)
+        .to_ansi()
+}
 
 #[derive(Debug)]
 enum UserError {
@@ -16,19 +94,18 @@ impl Error for UserError {}
 impl fmt::Display for UserError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            UserError::NoUser(e) => write!(f, "failed to get user: {}", e),
+            UserError::NoUser(e) => write!(f, "failed to get user: {}", e), 
         }
     }
 }
 
 fn get_user() -> Result<String, UserError> {
     let user = whoami::fallible::username().map_err(UserError::NoUser)?;
-    Ok(Color::Magenta.bold().paint(user).to_string())
-    // let dec = DecoratedString::new(user)
-    //     .bold()
-    //     .colored(Color::Magenta)
-    //     .to_ansi();
-    // Ok(dec)
+    let dec = DecoratedString::new(user)
+        .bold()
+        .colored(Color::Magenta)
+        .to_ansi();
+    Ok(dec)
 }
 
 #[derive(Debug)]
@@ -48,38 +125,112 @@ impl fmt::Display for HostnameError {
 
 fn get_hostname() -> Result<String, HostnameError> {
     let host = whoami::fallible::hostname().map_err(HostnameError::NoHost)?;
-    Ok(Color::Green.bold().paint(host).to_string())
-    // let dec = DecoratedString::new(host)
-    //     .bold()
-    //     .colored(Color::Green)
-    //     .to_ansi();
-    // Ok(dec)
+    let dec = DecoratedString::new(host)
+        .bold()
+        .colored(Color::Green)
+        .to_ansi();
+    Ok(dec)
 }
 
 fn get_cwd() -> String  {
     let cwd = env::var("PWD");
 
     if cwd.is_err() {
-        return Color::Red.bold().paint("!!!".to_string()).to_string();
-        // return DecoratedString::new("!!!".to_string())
-        //     .bold()
-        //     .colored(Color::Red)
-        //     .to_ansi();
+        return DecoratedString::new("!!!".to_string())
+            .bold()
+            .colored(Color::Red)
+            .to_ansi();
     }
 
     let mut cwd = cwd.unwrap();
-
+    
     if let Ok(home) = env::var("HOME") {
         if cwd.starts_with(&home) {
             cwd = cwd.replace(&home, "~");
         }
     }
 
-    Color::Blue.bold().paint(cwd).to_string()
-    // DecoratedString::new(cwd)
-    //     .bold()
-    //     .colored(Color::Blue)
-    //     .to_ansi()
+    DecoratedString::new(cwd)
+        .bold()
+        .colored(Color::Blue)
+        .to_ansi()
+}
+
+#[derive(Debug)]
+enum ShellError {
+    PsCommandFailed,
+    PsCommandTerminationUnsuccessful,
+    StdoutToStringFailed,
+}
+
+impl Error for ShellError {}
+
+impl fmt::Display for ShellError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::PsCommandFailed => write!(f, "the command: `ps -p $$ -o comm=` has failed"),
+            Self::PsCommandTerminationUnsuccessful => write!(f, "the command: `ps -p $$ -o comm=` was unsuccessful"),
+            Self::StdoutToStringFailed => write!(f, "stdout to string failed"),
+        }
+    }
+}
+
+fn get_shell() -> Result<String, ShellError> {
+    use std::process::Command;
+
+    // This spawns a new shell, so to look for the current shell type we need to go two layers up:
+    // 1 User shell session
+    // 2 prompt session
+    // 3 spawned sh to determine shell type of session 1
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("ps -p $(ps -o ppid= -p $(ps -o ppid= -p $$)) -o comm=")
+        .output()
+        .map_err(|_| ShellError::PsCommandFailed)?;
+
+    if !output.status.success() {
+        return Err(ShellError::PsCommandTerminationUnsuccessful);
+    }
+
+    let string = String::from_utf8(output.stdout)
+        .map_err(|_| ShellError::StdoutToStringFailed)?
+        .trim()
+        .to_string();
+
+    Ok(DecoratedString::new(string)
+        .bold()
+        .colored(Color::White)
+        .to_ansi())
+}
+
+#[derive(Debug)]
+enum StatusError {
+    NoExitCode,
+    CodeNotANumber
+}
+
+impl Error for StatusError {}
+
+impl fmt::Display for StatusError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::NoExitCode => write!(f, "no status/exit-code was found"),
+            Self::CodeNotANumber => write!(f, "a non-number exit code was returned"),
+        }
+    }
+}
+
+fn get_exit_code() -> Result<String, StatusError> {
+    let code = env::var("PROMPT_LAST_STATUS").map_err(|_| StatusError::NoExitCode)?;
+
+    let val = code.parse::<i32>().map_err(|_| StatusError::CodeNotANumber)?;
+    Ok(DecoratedString::new(code)
+        .bold()
+        .colored(match val {
+            0 => Color::Green,
+            _ => Color::Red,
+        })
+        .to_ansi())
 }
 
 #[derive(Debug)]
@@ -138,16 +289,17 @@ fn get_nix_shell() -> Result<String, NotInNixShell> {
         Unknown => "unknown",
     };
 
-    Ok(Color::White.bold().paint(format!("nix: {}", name)).to_string())
-    // Ok(DecoratedString::new(format!("nix: {}", name))
-    //     .bold()
-    //     .to_ansi())
+    Ok(DecoratedString::new(format!("nix: {}", name))
+        .bold()
+        .to_ansi())
 }
 
 #[derive(Debug)]
 enum MainError {
     User(UserError),
     Hostname(HostnameError),
+    Shell(ShellError),
+    Status(StatusError),
     NixShell(NotInNixShell),
 }
 
@@ -160,6 +312,14 @@ impl fmt::Display for MainError {
             },
             MainError::Hostname(e) => {
                 writeln!(f, "failed to get hostname info")?;
+                e
+            },
+            MainError::Shell(e) => {
+                writeln!(f, "failed to get shell info")?;
+                e
+            },
+            MainError::Status(e) => {
+                writeln!(f, "failed to get status")?;
                 e
             },
             MainError::NixShell(e) => {
@@ -180,18 +340,39 @@ impl fmt::Display for MainError {
     }
 }
 
-fn do_print(components: Vec<String>) {
-    let mut out = String::from("[");
-    out += &components.join("]-[");
-    out += "] -> ";
-    print!("{out}");
+fn do_print(mut components: Vec<String>) {
+    components.insert(0, "┌[".into());
+    for i in 1..components.len() - 1 {
+        components.insert(2 * i, "]-[".into());
+    }
+    components.push("]\n└> ".into());
+    for component in components {
+        print!("{component}");
+    }
 }
 
 fn main() {
+    // This program uses these environment variables:
+    //
+    // 1. `DEBUG_PROMPT`: 
+    //      1 => Print out debug stats
+    //      0 => No debug
+    // 2. `PROMPT_LAST_STATUS`:
+    //      Set to `$?` for an accurate last status.
+    //      Doesn't have to be set if no exit status is wanted
+    //
+    // Here is how to setup the prompt for zsh:
+    // ```.zshrc
+    // PROMPT="$(PROMPT_LAST_STATUS=$? ./path/to/prompt/binary))"
+    // ```
+
     let (oks, errors): (Vec<Result<_, MainError>>, Vec<_>) = vec![
+        Ok(get_time()),
         get_user().map_err(MainError::User),
         get_hostname().map_err(MainError::Hostname),
         Ok(get_cwd()),
+        get_shell().map_err(MainError::Shell),
+        get_exit_code().map_err(MainError::Status),
         get_nix_shell().map_err(MainError::NixShell),
     ]
     .into_iter()
